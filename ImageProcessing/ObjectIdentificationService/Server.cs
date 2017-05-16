62c2a5d5342d3665e70ceb52287bf03294fd0242
Eddie Löwenborg Forsberg <eddie.lowenborg@afconsult.com>
2017-05-16 16:57:53 +0200

    Serializer changed to json

--- ImageProcessing/ObjectIdentificationService/Server.cs
+++ ImageProcessing/ObjectIdentificationService/Server.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
+using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 using CommunicationProtocol;
+using Newtonsoft.Json;
 
 namespace ObjectIdentificationService
 {
@@ -27,24 +32,41 @@ namespace ObjectIdentificationService
             {
                 TcpClient client = listener.AcceptTcpClient();
                 Console.WriteLine("Connection accepted from " + client.Client.RemoteEndPoint);
-
-                NetworkStream stream = client.GetStream();
-
-                IFormatter formatter = new BinaryFormatter();
-
-                while (IsRunning)
+                using (StreamReader reader = new StreamReader(client.GetStream()))
+                using (StreamWriter writer = new StreamWriter(client.GetStream()))
                 {
-                    Console.WriteLine("Waiting for messages...");
-                    IdentificationRequest request = (IdentificationRequest)formatter.Deserialize(stream);
-                    
-                    string identifiedObject = library.IdentifyObject(request.Image);
+                    // format to json
+                    IFormatter formatter = new BinaryFormatter();
+                    while (IsRunning)
+                    {
+                        Console.WriteLine("Waiting for messages...");
+                        //IdentificationRequest request = (IdentificationRequest)formatter.Deserialize(stream);
+                        var request = JsonConvert.DeserializeObject<RequestMsg>(reader.ReadLine());
 
-                    IdentificationResponse resp = new IdentificationResponse();
-                    resp.Timestamp = DateTime.Now;
-                    resp.Object = identifiedObject;
+                        Bitmap bmp = new Bitmap(request.Width, request.Height, PixelFormat.Format8bppIndexed);
 
-                    formatter.Serialize(stream, resp);
-                    stream.Flush();
+                        var rect = new Rectangle(0, 0, request.Width, request.Height);
+                        var bmpData = bmp.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format8bppIndexed);
+
+                        var ptr = bmpData.Scan0;
+
+                        for (var i = 0; i < request.Height; i++)
+                        {
+                            Marshal.Copy(request.Pixels, i * request.Width, ptr, request.Width);
+                            ptr += bmpData.Stride;
+                        }
+
+                        bmp.UnlockBits(bmpData);
+
+                        string identifiedObject = library.IdentifyObject(bmp);
+
+                        var response = new ResponseMsg() {ObjectName = identifiedObject, Success = true};
+
+                        var responseJson = JsonConvert.SerializeObject(response);
+                        writer.WriteLine(responseJson);
+                        writer.Flush();
+
+                    }
                 }
             }
         }
@@ -52,6 +74,22 @@ namespace ObjectIdentificationService
         public void Stop()
         {
             IsRunning = false;
+        }
+
+        [Serializable]
+        private class RequestMsg
+        {
+            public Byte[] Pixels { get; set; }
+            public int Width { get; set; }
+            public int Height { get; set; }
+            
+        }
+
+        [Serializable]
+        private class ResponseMsg
+        {
+            public string ObjectName { get; set; }
+            public bool Success { get; set; }
         }
     }
 }
